277b12835c486eaf4e493c7061710cff2fbad8e0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-05-07 16:08:27 +0200

    Androids - Tools precept

---
+++ 1.4/Source/HarmonyPatches/Precepts/PawnDiedOrDownedThoughtsUtility_AppendThoughts_ForHumanlike.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System;
+
+namespace VREAndroids
+{
+    [HarmonyPatch(typeof(PawnDiedOrDownedThoughtsUtility), "AppendThoughts_ForHumanlike")]
+    public static class PawnDiedOrDownedThoughtsUtility_AppendThoughts_ForHumanlike_Patch
+    {
+        public static bool Prefix(Pawn victim, DamageInfo? dinfo)
+        {
+            
+            if (Faction.OfPlayerSilentFail?.ideos?.primaryIdeo?.HasPrecept(VREA_DefOf.VRE_Androids_Tools)==true && victim.IsAndroid())
+            {          
+                return false;
+            }
+            return true;
+        }
+    }
+}
