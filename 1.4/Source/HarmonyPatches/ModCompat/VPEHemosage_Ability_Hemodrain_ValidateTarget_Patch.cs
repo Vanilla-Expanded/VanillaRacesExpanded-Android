3b710663397ee9fa90e611cb0a4c227adfbb72ac
Taranchuk <nfhfyxer@gmail.com>
2023-06-15 23:58:35 +0300

    hemosage compat patches

---
+++ 1.4/Source/HarmonyPatches/ModCompat/VPEHemosage_Ability_Hemodrain_ValidateTarget_Patch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace VREAndroids
+{
+    [HarmonyPatch]
+    public static class VPEHemosage_Ability_Hemodrain_ValidateTarget_Patch
+    {
+        public static MethodInfo targetMethod;
+        public static bool Prepare()
+        {
+            targetMethod = AccessTools.Method("VPEHemosage.Ability_Hemodrain:ValidateTarget");
+            return targetMethod != null;
+        }
+        public static MethodBase TargetMethod()
+        {
+            return targetMethod;
+        }
+
+        [HarmonyPriority(int.MaxValue)]
+        public static bool Prefix(LocalTargetInfo target, bool showMessages = true)
+        {
+            if (target.Thing is Pawn pawn && pawn.IsAndroid())
+            {
+                if (showMessages)
+                {
+                    Messages.Message("VREA.CannotCastOnAndroid".Translate(pawn.Named("PAWN")), pawn, MessageTypeDefOf.RejectInput, historical: false);
+
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
