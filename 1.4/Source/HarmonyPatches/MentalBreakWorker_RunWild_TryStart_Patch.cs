4ea82112ed7a01ae9fb606f1dad5915bbc6dbc3d
Taranchuk <nfhfyxer@gmail.com>
2023-08-01 18:01:48 +0300

    prevent run wild mental breaks on androids + puppeteer compat

---
+++ 1.4/Source/HarmonyPatches/MentalBreakWorker_RunWild_TryStart_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace VREAndroids
+{
+    [HarmonyPatch(typeof(MentalBreakWorker_RunWild), "TryStart")]
+    public static class MentalBreakWorker_RunWild_TryStart_Patch
+    {
+        [HarmonyPriority(int.MaxValue)]
+        public static bool Prefix(Pawn pawn)
+        {
+            if (pawn.IsAndroid())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
