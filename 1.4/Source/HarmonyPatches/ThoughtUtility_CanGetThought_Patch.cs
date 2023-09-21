0407467fd93619953f05bbf80e4d69466ad5185f
Taranchuk <nfhfyxer@gmail.com>
2023-09-21 19:42:30 +0300

    mood fix

---
+++ 1.4/Source/HarmonyPatches/ThoughtUtility_CanGetThought_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VREAndroids
+{
+    [HarmonyPatch(typeof(ThoughtUtility), "CanGetThought")]
+    public static class ThoughtUtility_CanGetThought_Patch
+    {
+        [HarmonyPriority(int.MaxValue)]
+        public static bool Prefix(Pawn pawn)
+        {
+            if (pawn.IsAndroid() && pawn.IsAwakened() is false)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
