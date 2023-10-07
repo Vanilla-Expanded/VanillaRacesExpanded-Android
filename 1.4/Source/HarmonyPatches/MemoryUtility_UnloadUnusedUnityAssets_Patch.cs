96fa981602db20c189f297420d148cc5ad0ee776
Taranchuk <nfhfyxer@gmail.com>
2023-10-07 23:55:58 +0300

    free memory space job fix

---
+++ 1.4/Source/HarmonyPatches/MemoryUtility_UnloadUnusedUnityAssets_Patch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.Profile;
+
+namespace VREAndroids
+{
+    [HarmonyPatch(typeof(MemoryUtility), "UnloadUnusedUnityAssets")]
+    public static class MemoryUtility_UnloadUnusedUnityAssets_Patch
+    {
+        public static void Postfix()
+        {
+            Building_AndroidStand.stands.Clear();
+        }
+    }
+}
