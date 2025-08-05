cab08b4ca52492c6b5227771ad6a4260ef30032b
SokyranTheDragon <exploder1531@gmail.com>
2025-08-05 17:52:39 +0200

    Fix errors with Ideology when right-clicking an android

---
+++ 1.6/Source/HarmonyPatches/CompBiosculpterPod_FindPodFor_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VREAndroids
+{
+    [HarmonyPatch(typeof(CompBiosculpterPod), nameof(CompBiosculpterPod.FindPodFor))]
+    public static class CompBiosculpterPod_FindPodFor_Patch
+    {
+        public static bool Prefix(Pawn traveller)
+        {
+            if (traveller.IsAndroid())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
