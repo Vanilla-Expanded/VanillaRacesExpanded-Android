8bfa39694fbfd19d93954519323510176db12c05
SokyranTheDragon <exploder1531@gmail.com>
2026-02-28 17:34:21 +0100

    Update QuestNode_Root_WandererJoinAbasia:GeneratePawn for latest unstable

--- 1.6/Source/HarmonyPatches/QuestNode_Root_WandererJoinAbasia_GeneratePawn_Patch.cs
+++ 1.6/Source/HarmonyPatches/QuestNode_Root_WandererJoinAbasia_GeneratePawn_Patch.cs
@@ -3,18 +3,25 @@ using RimWorld;
 using RimWorld.QuestGen;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Verse;
 
 namespace VREAndroids
 {
-    [HarmonyPatch(typeof(QuestNode_Root_WandererJoinAbasia), "GeneratePawn")]
+    [HarmonyPatch]
     public static class QuestNode_Root_WandererJoinAbasia_GeneratePawn_Patch
     {
+        public static MethodBase TargetMethod()
+        {
+            return typeof(QuestNode_Root_WandererJoinAbasia).DeclaredMethod(nameof(QuestNode_Root_WandererJoinAbasia.GeneratePawn_NewTemp))
+                   ?? typeof(QuestNode_Root_WandererJoinAbasia).DeclaredMethod(nameof(QuestNode_Root_WandererJoinAbasia.GeneratePawn));
+        }
+
         public static void Prefix()
         {
             PawnGenerator_XenotypesAvailableFor_Patch.preventAndroidGeneration = true;
         }
-        public static void Postfix()
+        public static void Finalizer()
         {
             PawnGenerator_XenotypesAvailableFor_Patch.preventAndroidGeneration = false;
         }
